27af409e5eec4b7a6ef4d457d91fae95c6634a9a
josue1471515 <32602151+josue1471515@users.noreply.github.com>
2017-10-13 09:13:31 -0400

    update models

--- server/data_layer/models.py
+++ server/data_layer/models.py
@@ -10,28 +10,6 @@ Base = declarative_base()
 
 class Developer(Base):
     __tablename__ = 'developer'
-
-    id = Column(Integer, primary_key=True)
-    name = Column(String(50), nullable=False)
-    description = Column(String(255), nullable=False)
-    meeting_days = Column(Integer, nullable=False)
-    enabled = Column(Boolean, nullable=False, default=True)
-    timestamp_created = Column(DateTime, nullable=False, default=datetime.datetime.utcnow)
-    timestamp_modif = Column(DateTime, nullable=False)
-
-    projects = relationship('Project')
-
-
-class ProjectDeveloper(Base):
-    __tablename__ = 'project_developer'
-
-    fk_proj = Column(Integer, ForeignKey('developer.id'), primary_key=True)
-    fk_dev = Column(Integer, ForeignKey('project.id'), primary_key=True)
-
-
-class Project(Base):
-    __tablename__ = 'project'
-
     id = Column(Integer, primary_key=True)
     first_name = Column(String(50), nullable=False)
     last_name = Column(String(50), nullable=False)
@@ -42,6 +20,24 @@ class Project(Base):
     password = Column(String(100), nullable=False)
     fk_role = Column(Integer, ForeignKey('role_dev.id'), primary_key=True)
     enabled = Column(Boolean, nullable=False, default=True)
+    projects = relationship('Project')
+
+
+class ProjectDeveloper(Base):
+    __tablename__ = 'project_developer'
+    fk_proj = Column(Integer, ForeignKey('developer.id'), primary_key=True)
+    fk_dev = Column(Integer, ForeignKey('project.id'), primary_key=True)
+    hours_worked = Column(Integer, nullable=False)
+
+class Project(Base):
+      __tablename__ = 'project'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(50), nullable=False)
+    description = Column(String(255), nullable=False)
+    estimated_hours = Column(Integer, nullable=False)
+    enabled = Column(Boolean, nullable=False, default=True)
+    timestamp_created = Column(DateTime, nullable=False, default=datetime.datetime.utcnow)
+    timestamp_modif = Column(DateTime, nullable=False)
 
 
 class RoleDev(Base):
